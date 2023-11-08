c9b34bacd3d6f181567b3f1245309461d0e4768b
nanthups <nanthulogezy@gmail.com>
2023-11-09 00:14:55 +0530

    Added: registration pages

---
+++ src/pages/auth/register/account.vue
@@ -0,0 +1,72 @@
+<template>
+  <section>
+    <div class="pb-10 pb-lg-15">
+      <h4 class="d-flex align-items-center text-gray-900">
+        Choose Account Type
+        <a href="#" class="text-sm text-muted text-primary-hover font-semibold me-2 d-none d-md-block ms-1"
+        data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top"
+        data-bs-content="Choose your account type to get started. Are you an agency looking to hire or a candidate seeking opportunities?">
+        <BootstrapIcon name="exclamation-circle-fill" />
+      </a>
+    </h4>
+
+    <div class="text-muted small">
+      If you need more info, please check out
+      <a href="#" class="link-primary">Help Page</a>.
+    </div>
+  </div>
+
+  <div class="position-relative">
+    <div class="row">
+      <div class="col-lg-6 my-2">
+        <input type="radio" class="btn-check" name="account_type" v-model="account" value="agency" id="account-type-agency">
+        <label class="btn btn-outline-light border border-dashed p-6 d-flex align-items-center mb-10" for="account-type-agency">
+          <BootstrapIcon name="building-fill" class="text-muted me-5" />
+          <span class="text-gray-900 d-block">
+            Agency
+          </span>
+        </label>
+      </div>
+
+      <div class="col-lg-6 my-2">
+        <input type="radio" class="btn-check" name="account_type" v-model="account" value="candidate" id="account-type-candidate">
+        <label class="btn btn-outline-light border border-dashed p-6 d-flex align-items-center mb-10" for="account-type-candidate">
+          <BootstrapIcon name="person-circle" class="text-muted me-5" />
+          <span class="text-gray-900 d-block">
+            Candidate
+          </span>
+        </label>
+      </div>
+    </div>
+  </div>
+
+  <div class="d-flex align-items-center justify-content-end">
+    <button type="button" class="btn btn-primary btn-sm" v-on:click.prevent="navigateComponent">
+      Continue
+      <BootstrapIcon name="arrow-right" class="ms-2" />
+    </button>
+  </div>
+</section>
+</template>
+
+<script setup lang="ts">
+const account = ref<String>("agency");
+
+const emit = defineEmits(['navigateComponent'])
+
+const navigateComponent = () => {
+  emit('navigateComponent', account.value)
+};
+</script>
+
+<style lang="scss" scoped>
+input[type="radio"]:checked+label {
+  background-color: rgba(var(--x-surface-secondary-rgb), 1);
+  border: var(--x-border-width) var(--x-border-style) var(--x-primary) !important;
+
+  i,
+  span {
+    color: rgba(var(--x-primary-rgb), var(--x-text-opacity)) !important;
+  }
+}
+</style>
---
+++ src/pages/auth/register/candidate.vue
@@ -0,0 +1,80 @@
+<template>
+  <form method="POST" action="/">
+    <div class="row g-5">
+      <div class="col-xl-6">
+        <div class="mb-5">
+          <label class="form-label" for="first_name">
+            First Name
+          </label>
+          <input class="form-control" id="first_name" type="text" name="first_name">
+        </div>
+      </div>
+      <div class="col-xl-6">
+        <div class="mb-5">
+          <label class="form-label" for="last_name">
+            Last Name
+          </label>
+          <input class="form-control" id="last_name" type="text" name="last_name">
+        </div>
+      </div>
+    </div>
+
+    <div class="mb-5">
+      <label class="form-label" for="email">
+        Email
+      </label>
+      <input class="form-control" id="email" type="email" name="email">
+    </div>
+
+    <div class="row">
+      <div class="col-lg-12">
+        <div>
+          <label class="form-label" for="password">
+            Password
+          </label>
+          <input class="form-control" id="password" type="password" name="password">
+        </div>
+      </div>
+    </div>
+
+    <div class="my-6">
+      <div class="form-check mb-5">
+        <input class="form-check-input" type="checkbox" name="terms" id="checkTerms">
+        <label class="form-check-label pl-2" for="checkTerms">
+          By creating an account means you agree to the <a href="https://webpixels.io/terms"
+          target="_blank" class="font-bold text-heading">Terms and Conditions</a>, and our
+          <a href="https://webpixels.io/privacy" target="_blank"
+          class="font-bold text-heading">Privacy Policy</a>
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" name="newsletter" id="checkNewsletter">
+        <label class="form-check-label pl-2" for="checkNewsletter">
+          Send me email with hot tips, awesome news, or both.
+        </label>
+      </div>
+    </div>
+
+    <div class="d-flex align-items-center justify-content-between mt-6">
+      <button type="submit" class="btn btn-secondary btn-sm" v-on:click.prevent="navigateComponent">
+        <BootstrapIcon name="arrow-left" class="me-2" />
+        Back
+      </button>
+      <button type="submit" class="btn btn-primary">
+        Register
+      </button>
+    </div>
+  </form>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['navigateComponent'])
+
+const navigateComponent = () => {
+  emit('navigateComponent', 'account')
+};
+</script>
+
+<style scoped>
+/* Add your styling for Step 1 */
+</style>
